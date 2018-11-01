dec498842943cd7dce47c766bde195d854ab71a5
xecutioner0110 <44304004+xecutioner0110@users.noreply.github.com>
2018-11-01 18:22:57 +0300

    Add files via upload

---
+++ events/guildCreate.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+const girismesaj = [
+  '**TheOrder sunucunuza eklendi!**',
+  '**TheOrder** sunucunuzdaki insanlara kolaylıklar sağlar.',
+  'Bot Batur O. tarafından geliştirilmektedir.',
+  'Botumuzun özelliklerini öğrenmek için !yardım komutunu kullanabilirsin.',
+  '**ÖNEMLİ:** Botun kullanması için mod-log kanalı açın ve deneme için',
+  't!uyarı komutunu kullanın.',
+  '',
+]
+
+client.on('guildCreate', guild => {
+    const generalChannel = guild.defaultChannel
+    generalChannel.sendMessage(girismesaj)
+	client.user.setGame(prefix + 'yardım | ' + client.guilds.size + ' sunucu | ' + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' kullanıcı');
+})
